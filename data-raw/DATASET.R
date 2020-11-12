f3c07da88251da6bcd2f630300987c1da318a645
vgherard <vgherard@sissa.it>
2020-11-12 20:45:03 +0100

    Update data according to new class structure

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,6 +2,6 @@
 
 twitter_dict <- names(get_word_freqs(twitter_train))[1:1000]
 twitter_freqs <- get_kgram_freqs(twitter_train, N = 3, twitter_dict)
-twitter_preds <- build_sbo_preds(twitter_freqs)
+twitter_predtable <- build_predtable(twitter_freqs)
 
-usethis::use_data(twitter_dict, twitter_freqs, twitter_preds, overwrite = TRUE)
+usethis::use_data(twitter_dict, twitter_freqs, twitter_predtable, overwrite = TRUE)
