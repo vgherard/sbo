f6771f9efc3ffd3da0fc30d3669d36eb72647286
vgherard <vgherard@sissa.it>
2020-11-17 15:26:08 +0100

    Update data after fix in `build_pps()` for ignored words

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,7 @@
 ## code to prepare `DATASET` dataset goes here
 
+library(sbo)
+
 twitter_dict <- names(get_word_freqs(twitter_train))[1:1000]
 twitter_freqs <- get_kgram_freqs(twitter_train, N = 3, twitter_dict)
 twitter_predtable <- build_predtable(twitter_freqs)
