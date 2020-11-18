a5c4fa23c6dad1351160118630d5925ff8eeb911
vgherard <vgherard@sissa.it>
2020-11-18 12:18:18 +0100

    summary() methods basic tests

---
+++ tests/testthat/test-summary_sbo_predictor.R
@@ -0,0 +1,22 @@
+context("summary.sbo_predictor")
+
+p <- load_predictor(twitter_predtable)
+
+test_that("returns invisibly the correct value", {
+        capture_output({
+                expect_invisible(summary(p))
+                expect_identical(summary(p), p)
+                },
+                print = F)
+})
+
+test_that("Prints a title, an empty line and some text", {
+        output <- capture_output_lines(summary(p), print = F)
+        expect_gt(length(output), 2)
+        title <- "Next-word text predictor from Stupid Back-off N-gram model"
+        expect_identical(output[1], title)
+        expect_identical(output[2], "")
+        expect_true(output[3] != "")
+})
+
+rm(p)
