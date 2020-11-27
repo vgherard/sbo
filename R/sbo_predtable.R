3371f05561148b350618a31566e2cf8ac83f4672
vgherard <vgherard@sissa.it>
2020-11-27 10:55:15 +0100

    Expand test suite for sbo_predtable; clarify errors on 'L' too big

--- R/sbo_predtable.R
+++ R/sbo_predtable.R
@@ -1,16 +1,18 @@
 #' @rdname sbo_predictions
 #' @export
 sbo_predtable <- function(object, lambda = 0.4, L = 3L, filtered = "<UNK>", ...){
+        error <- function(msg)
+                rlang::abort(class = "sbo_domain_error", message = msg)  
         if (!is.numeric(lambda) || length(lambda) != 1)
-                stop("'lambda' must be a length one numeric.")
+                error("'lambda' must be a length one numeric.")
         if (lambda < 0 || lambda > 1)
-                stop("'lambda' must be in the interval [0,1].")
+                error("'lambda' must be in the interval [0,1].")
         if (!is.numeric(L) || length(L) != 1)
-                stop("'L' must be a length one integer.")
+                error("'L' must be a length one integer.")
         if (L < 1) 
-                stop("'L' must be greater than one.")
+                error("'L' must be greater than one.")
         if (!is.character(filtered)) 
-                stop("'filtered' must be a character vector")
+                error("'filtered' must be a character vector")
         UseMethod("sbo_predtable")
 }
 
@@ -49,14 +51,22 @@ sbo_predtable.sbo_kgram_freqs <- function(object,
         N <- attr(object, "N")
         dict <- attr(object, "dict")
         L <- as.integer(L)
-        if (L > length(dict))
-                stop("'L' must be less than the dictionary size.")
         
         EOS <- attr(object, "EOS")
         .preprocess <- attr(object, ".preprocess")
         
-        filtered <- match(filtered, table = c(dict, "<EOS>", "<UNK>"), 
-                          nomatch = -1)
+        filtered <- unique(intersect(filtered, c(dict, "<EOS>", "<UNK>")))
+        max_L <- length(dict) + 2 - length(filtered)
+        filtered <- match(filtered, table = c(dict, "<EOS>", "<UNK>"))
+        
+        if (max_L == 0) rlang::abort(
+                class = "sbo_domain_error",
+                message = "No words allowed for prediction!"
+                )
+        if (L > max_L) rlang::abort(
+                class = "sbo_domain_error",
+                message = paste0("'L' must be less than ", max_L)
+                )
         
         extract_preds <- . %>% 
                 select(-.data$score) %>%
--- tests/testthat/test-sbo_predtable.R
+++ tests/testthat/test-sbo_predtable.R
@@ -19,3 +19,42 @@ test_that("return value has the correct structure", {
         expect_true(is.character(attr(predtable, "EOS")))
         expect_true(is.function(attr(predtable, ".preprocess")))
 })
+
+test_that("different methods are consistent", {
+        input <- c("a a b a", "a b b a", "a c b", "b c a a b")
+        predtable_char <- sbo_predtable(input, N = 3, dict = c("a", "b", "c"))
+        freqs <- kgram_freqs(input, N = 3, dict = c("a", "b", "c"))
+        predtable_freqs <- sbo_predtable(freqs)
+        expect_identical(predtable_char, predtable_freqs)
+})
+
+
+
+# Used in the following tests
+
+input <- c("a a b a", "a b b a", "a c b", "b c a a b")
+freqs <- kgram_freqs(input, N = 3, dict = c("a", "b", "c"))
+expect_domain_error <- function(x) expect_error(x, class = "sbo_domain_error")
+
+test_that("Errors on invalid lambda", {
+        expect_domain_error(sbo_predtable(freqs, lambda = "Zero"))
+        expect_domain_error(sbo_predtable(freqs, lambda = c(0.2, 0.4)))
+        expect_domain_error(sbo_predtable(freqs, lambda = -0.4))
+        expect_domain_error(sbo_predtable(freqs, lambda = 1.2))
+})
+
+test_that("Errors on invalid L", {
+        expect_domain_error(sbo_predtable(freqs, L = "All"))
+        expect_domain_error(sbo_predtable(freqs, L = c(1,2,3)))
+        expect_domain_error(sbo_predtable(freqs, L = 0))
+        expect_domain_error(sbo_predtable(freqs, L = 5)) # Too many predictions!
+})
+
+test_that("Errors on invalid filtered", {
+        expect_domain_error(sbo_predtable(freqs, filtered = c(1,2,3)))
+})
+
+test_that("Errors on L_max == 0", {
+        filtered = c("a", "b", "c", "d", "e", "<UNK>", "<EOS>")
+        expect_domain_error(sbo_predtable(freqs, filtered = filtered))
+})
