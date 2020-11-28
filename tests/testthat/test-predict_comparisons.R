4a323bf257357fc432e44353c48d4cdd76a6898d
vgherard <vgherard@sissa.it>
2020-11-28 11:31:18 +0100

    Added random test checking also sbo_predictor.character method

--- tests/testthat/test-predict_comparisons.R
+++ tests/testthat/test-predict_comparisons.R
@@ -1,5 +1,27 @@
 context("comparing predict.sbo_kgram_freqs() and predict.sbo_predictor()")
 
+test_that("predictions agree on simple example", {
+        train <- paste(sample(letters, 10000, replace = T, prob = rnorm(26)^2), 
+                       collapse = " ")
+        dict <- sample(letters, 13)
+        lambda <- 0.4
+        L <- 1
+        freqs <- kgram_freqs(corpus = train, N = 3, dict = dict)
+        p <- sbo_predictor(object = train, N = 3, dict = dict, L = L)
+        
+        N_tests <- 20
+        w1 <- sample(dict, N_tests, replace = T)
+        w2 <- sample(dict, N_tests, replace = T) 
+        for (input in paste(w1, w2)) {
+                from_freqs <- predict(freqs, input)$completion[1:L]
+                from_preds <- predict(p, input)
+                
+                expect_identical(from_freqs, from_preds)
+        }
+        
+})
+
+# Variables used in the following tests
 p <- sbo_predictor(twitter_predtable)
 L <- attr(p, "L")
 
@@ -28,3 +50,5 @@ for (input in inputs) {
                 expect_identical(from_freqs, from_preds)
         })
 }
+
+rm(p, L)
