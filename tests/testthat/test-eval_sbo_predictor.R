7e2546ab696b7fa3bd9f919d93cbca92e816ee8b
vgherard <vgherard@sissa.it>
2020-12-03 17:21:51 +0100

    Test correct output for different values of L

--- tests/testthat/test-eval_sbo_predictor.R
+++ tests/testthat/test-eval_sbo_predictor.R
@@ -8,8 +8,9 @@ test_that("doesn't throw errors with twitter_test sample",{
 
 test_that("output has the correct structure",{
         n_test <- 100
+        L <- 2
         p <- sbo_predictor(twitter_predtable)
-        p_eval <- eval_sbo_predictor(p, twitter_test[1:100])
+        p_eval <- eval_sbo_predictor(p, twitter_test[1:100], L = L)
         expect_s3_class(p_eval, class(tibble()))
         expect_true(nrow(p_eval) == n_test)
         classes <- list(input = "character", 
@@ -18,12 +19,13 @@ test_that("output has the correct structure",{
                         correct = "logical")
         expect_identical(lapply(p_eval, function(x) class(x)[[1]]), 
                          classes)
+        expect_equal(dim(p_eval[["preds"]]), c(n_test, L))
 })
 
 test_that("correctly identifies correct prediction",{
         n_test <- 100
         p <- sbo_predictor(twitter_predtable)
-        p_eval <- eval_sbo_predictor(p, twitter_test[1:100])
+        p_eval <- eval_sbo_predictor(p, twitter_test[1:n_test], L = 2)
         correct <- logical(n_test)
         for (i in seq_len(n_test)) {
                 correct[[i]] <- p_eval[["true"]][[i]] %in% p_eval[["preds"]][i, ]
